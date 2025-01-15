b7c566848aaa450230118f5a6b42a95c6be5fd12
amaraa-11 <amaraamygmarjaw@gmail.com>
2025-01-15 17:51:36 +0800

    food delivery day3

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -1,3 +1,56 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+type CategoryType = {
+  CategoryName: string;
+  _id: number;
+  value: string;
+};
+
 export default function Home() {
-  return <div></div>;
+  const [categories, setCategory] = useState<CategoryType[]>([]);
+  const [value, setValue] = useState<string>("");
+  async function foodCategory() {
+    const res = await fetch(`http://localhost:4000/food-category`, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await res.json();
+    setCategory(data);
+  }
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  };
+  useEffect(() => {
+    foodCategory();
+  }, []);
+
+  const addFood = async () => {
+    const newFood = { CategoryName: value };
+    const response = await fetch(`http://localhost:4000/add-movie`, {
+      method: "POST",
+      body: JSON.stringify(newFood),
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    });
+
+    const foodData = await response.json();
+    setCategory((prevCategory) => [...prevCategory, foodData]);
+  };
+  return (
+    <div>
+      {categories?.map((category) => (
+        <div key={category._id} className="flex justify-row-reverse ">
+          {category.CategoryName}
+        </div>
+      ))}
+    </div>
+  );
 }
